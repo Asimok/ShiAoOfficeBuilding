e7d7ee9ad1d7cb380e9ea6a5685bcd8885f89249
LeoMachine <1962976634@qq.com>
2019-04-03 21:04:48 +0800

    LISTVIEW更新

--- app/src/main/java/com/example/shiaoofficebuilding/MainActivity.java
+++ app/src/main/java/com/example/shiaoofficebuilding/MainActivity.java
@@ -8,7 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +30,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+
 public class MainActivity extends AppCompatActivity {
     TextView tv;
     private ListView roomlv;
@@ -35,18 +38,19 @@ public class MainActivity extends AppCompatActivity {
     private  String count;
     MyAdapter adapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchroom_lv_layout);
         roomInfoForAdapter = new ArrayList<roomlistAdapterInfo>();
         roomlv = findViewById(R.id.searchroomlv);
-        sendRequest();//get方法请求获取数据
+        getRoomlist();//get方法请求获取数据
+        getBulidlist();
 
     }
 
-    private void sendRequest() {
-
+    private void getRoomlist() {
 
         OkHttpClient okhttpClient = new OkHttpClient();
         final Request request = new Request.Builder()
@@ -91,9 +95,9 @@ public class MainActivity extends AppCompatActivity {
                         JSONObject jsonObj2 = jsonArray.getJSONObject(i);
                         //房间号
                         String roomnum = jsonObj2.getString("roomnum");
-                        //房间状态
+                        //房间
                         String statusname = jsonObj2.getString("statusname");
-                        //使用状态
+                        //使用
                         String usetypename = jsonObj2.getString("usetypename");
 
                         showRoomlistResult(roomnum,statusname,usetypename,pos);
@@ -106,7 +110,63 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void getBulidlist() {
 
+
+        OkHttpClient okhttpClient = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url("http://47.93.103.150/OfficeWebApp/Office/service/GetJsonDataX.ashx?opera=buildlist")
+                .get()
+                .build();
+        Call call = okhttpClient.newCall(request);
+        call.enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "连接服务器失败！", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                String res = response.body().string();//获取到传过来的字符串
+                try {
+                    JSONObject jsonObj = new JSONObject(res);
+
+                    //获取结果
+                    String result = jsonObj.getString("result");
+                    Log.d("aa", result);
+                    //获取到的数据数量
+                    count = jsonObj.getString("count");
+                    Log.d("aa", count);
+
+                    //获取到的房间数据
+                    String roomlist = jsonObj.getString("data");
+                    //转换成JSON数组
+                    JSONArray jsonArray = new JSONArray(roomlist);
+                    for (int i = 0; i < Integer.parseInt(count); i++) {
+                        int pos =i;
+                        //遍历获取数据
+                        JSONObject jsonObj2 = jsonArray.getJSONObject(i);
+                        //写字楼名称
+                        String name = jsonObj2.getString("name");
+
+                        showBulidlist(name);
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
     public void showRoomlistResult(final String res,final String statusname,final String usetypename,final  int pos )
     //封装遍历的数据
     {
@@ -119,14 +179,26 @@ public class MainActivity extends AppCompatActivity {
                 mapx.setStatusname(statusname);
                 mapx.setUsetypename(usetypename);
                 roomInfoForAdapter.add(mapx);
+                //数据适配器
                 adapter = new MyAdapter(MainActivity.this,R.layout.showroomlist,roomInfoForAdapter);
                 roomlv.setAdapter(adapter);
             }
         });
     }
+    public void showBulidlist(final String name )
+    //封装遍历的数据
+    {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String bulidname="";
+                bulidname=bulidname+name;
+                setTitle(bulidname);
+            }
+        });
+    }
 
 
-//数据适配器
 
 
 
--- app/src/main/java/com/example/shiaoofficebuilding/MyAdapter.java
+++ app/src/main/java/com/example/shiaoofficebuilding/MyAdapter.java
@@ -1,11 +1,14 @@
 package com.example.shiaoofficebuilding;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,10 +37,12 @@ public class MyAdapter extends BaseAdapter{
     }
     @Override
     public int getCount() {
-        if(list.size()%3>0) {
-            return list.size()/3+1;
+        if(list.size()%4>0) {
+            Log.d("cc","if(list.size()%4>0) ");
+            return list.size()/4+1;
+
         } else {
-            return list.size()/3;
+            return list.size()/4;
         }
     }
     @Override
@@ -57,46 +62,294 @@ public class MyAdapter extends BaseAdapter{
             vh.tv1=(TextView)convertView.findViewById(R.id.roomnum1);
             vh.tv2=(TextView)convertView.findViewById(R.id.roomnum2);
             vh.tv3=(TextView)convertView.findViewById(R.id.roomnum3);
+            vh.tv4=(TextView)convertView.findViewById(R.id.roomnum4);
+            vh.tv11=(TextView)convertView.findViewById(R.id.statusname1);
+            vh.tv12=(TextView)convertView.findViewById(R.id.statusname2);
+            vh.tv13=(TextView)convertView.findViewById(R.id.statusname3);
+            vh.tv14=(TextView)convertView.findViewById(R.id.statusname4);
+            vh.tv21=(TextView)convertView.findViewById(R.id.usetypename1);
+            vh.tv22=(TextView)convertView.findViewById(R.id.usetypename2);
+            vh.tv23=(TextView)convertView.findViewById(R.id.usetypename3);
+            vh.tv24=(TextView)convertView.findViewById(R.id.usetypename4);
+            vh.parent=(RelativeLayout) convertView.findViewById(R.id.parent);
+            vh.parent2=(RelativeLayout) convertView.findViewById(R.id.parent2);
+            vh.parent3=(RelativeLayout) convertView.findViewById(R.id.parent3);
+            vh.parent4=(RelativeLayout) convertView.findViewById(R.id.parent4);
             convertView.setTag(vh);
         }else {
             vh = (ViewHolder)convertView.getTag();
         }
-        int distance =  list.size() - position*3;
-        int cellCount = distance >= 3? 3:distance;
-        final List<roomlistAdapterInfo> itemList = list.subList(position*3,position*3+cellCount);
+        int distance =  list.size() - position*4;
+        int cellCount = distance >= 4? 4:distance;
+
+        final List<roomlistAdapterInfo> itemList = list.subList(position*4,position*4+cellCount);
+        if(!itemList.isEmpty())
+        {
+            Log.d("cc","进入 "+position);
+
         if (itemList.size() >0) {
             vh.tv1.setText(itemList.get(0).getRoomNumber());
+            vh.tv11.setText(itemList.get(0).getStatusname());
+
+            switch (itemList.get(0).getStatusname()) {
+                case "自用":
+                    vh.tv1.setTextColor(Color.GREEN);
+                    vh.tv11.setTextColor(Color.GREEN);
+                    vh.tv21.setTextColor(Color.GREEN);
+                    break;
+                case "出租":
+                    vh.tv1.setTextColor(Color.GRAY);
+                    vh.tv11.setTextColor(Color.GRAY);
+                    vh.tv21.setTextColor(Color.GRAY);
+                    break;
+                case "":
+                    vh.tv11.setText("---");
+                    vh.tv1.setTextColor(Color.WHITE);
+                    vh.tv11.setTextColor(Color.WHITE);
+                    vh.tv21.setTextColor(Color.WHITE);
+                    break;
+                case "自住":
+                    vh.tv1.setTextColor(Color.LTGRAY);
+                    vh.tv11.setTextColor(Color.LTGRAY);
+                    vh.tv21.setTextColor(Color.LTGRAY);
+                    break;
+                default:
+                    vh.tv1.setTextColor(Color.parseColor("#3abfd1"));
+                    vh.tv11.setTextColor(Color.parseColor("#3abfd1"));
+                    vh.tv21.setTextColor(Color.parseColor("#3abfd1"));
+            }
+            vh.tv21.setText(itemList.get(0).getUsetypename());
+            switch (itemList.get(0).getUsetypename()) {
+                case "公司":
+                    vh.parent.setBackgroundColor(Color.BLUE);
+                    break;
+                case "库房":
+                    vh.parent.setBackgroundColor(Color.YELLOW);
+                    break;
+                case "宿舍":
+                    vh.parent.setBackgroundColor(Color.CYAN);
+                    break;
+                case "":
+                    vh.tv21.setText("---");
+                    vh.parent.setBackgroundColor(Color.RED);
+                    break;
+
+
+            }
             vh.tv1.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(context, itemList.get(0).getRoomNumber(), Toast.LENGTH_SHORT).show();
                 }
             });
-            if (itemList.size() >1){
+            if (itemList.size() > 1) {
                 vh.tv2.setVisibility(View.VISIBLE);
+                vh.tv12.setVisibility(View.VISIBLE);
+                vh.tv22.setVisibility(View.VISIBLE);
                 vh.tv2.setText(itemList.get(1).getRoomNumber());
+
+                vh.tv12.setText(itemList.get(1).getStatusname());
+
+                switch (itemList.get(1).getStatusname().trim()) {
+                    case "自用":
+                        vh.tv2.setTextColor(Color.GREEN);
+                        vh.tv12.setTextColor(Color.GREEN);
+                        vh.tv22.setTextColor(Color.GREEN);
+                        break;
+                    case "出租":
+                        vh.tv2.setTextColor(Color.GRAY);
+                        vh.tv12.setTextColor(Color.GRAY);
+                        vh.tv22.setTextColor(Color.GRAY);
+                        break;
+                    case "自住":
+                        vh.tv2.setTextColor(Color.LTGRAY);
+                        vh.tv12.setTextColor(Color.LTGRAY);
+                        vh.tv22.setTextColor(Color.LTGRAY);
+                        break;
+                    case "":
+                        vh.tv12.setText("---");
+                        vh.tv2.setTextColor(Color.WHITE);
+                        vh.tv12.setTextColor(Color.WHITE);
+                        vh.tv22.setTextColor(Color.WHITE);
+                        break;
+                    default:
+                        vh.tv2.setTextColor(Color.parseColor("#3abfd1"));
+                        vh.tv12.setTextColor(Color.parseColor("#3abfd1"));
+                        vh.tv22.setTextColor(Color.parseColor("#3abfd1"));
+
+                }
+
+                vh.tv22.setText(itemList.get(1).getUsetypename());
+
+
+                switch (itemList.get(1).getUsetypename().trim()) {
+                    case "公司":
+                        vh.parent2.setBackgroundColor(Color.BLUE);
+                        break;
+                    case "库房":
+                        vh.parent2.setBackgroundColor(Color.YELLOW);
+                        break;
+                    case "宿舍":
+                        vh.parent2.setBackgroundColor(Color.CYAN);
+                        break;
+                    case "":
+                        vh.tv22.setText("---");
+                        vh.parent2.setBackgroundColor(Color.RED);
+                        break;
+
+                }
                 vh.tv2.setOnClickListener(new OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Toast.makeText(context, itemList.get(1).getRoomNumber(), Toast.LENGTH_SHORT).show();
                     }
                 });
-            }else{
+            } else {
                 vh.tv2.setVisibility(View.INVISIBLE);
+                vh.tv21.setVisibility(View.INVISIBLE);
+                vh.tv22.setVisibility(View.INVISIBLE);
+
+
             }
 
-            if (itemList.size() >2){
+            if (itemList.size() > 2) {
                 vh.tv3.setVisibility(View.VISIBLE);
+                vh.tv13.setVisibility(View.VISIBLE);
+                vh.tv23.setVisibility(View.VISIBLE);
                 vh.tv3.setText(itemList.get(2).getRoomNumber());
+
+                vh.tv13.setText(itemList.get(2).getStatusname());
+                switch (itemList.get(2).getStatusname().trim()) {
+                    case "自用":
+                        vh.tv3.setTextColor(Color.GREEN);
+                        vh.tv13.setTextColor(Color.GREEN);
+                        vh.tv23.setTextColor(Color.GREEN);
+                        break;
+                    case "出租":
+                        vh.tv3.setTextColor(Color.GRAY);
+                        vh.tv13.setTextColor(Color.GRAY);
+                        vh.tv23.setTextColor(Color.GRAY);
+                        break;
+                    case "自住":
+                        vh.tv3.setTextColor(Color.LTGRAY);
+                        vh.tv13.setTextColor(Color.LTGRAY);
+                        vh.tv23.setTextColor(Color.LTGRAY);
+                        break;
+                    case "":
+                        vh.tv13.setText("---");
+                        vh.tv3.setTextColor(Color.WHITE);
+                        vh.tv13.setTextColor(Color.WHITE);
+                        vh.tv23.setTextColor(Color.WHITE);
+                        break;
+                    default:
+                        vh.tv3.setTextColor(Color.parseColor("#3abfd1"));
+                        vh.tv13.setTextColor(Color.parseColor("#3abfd1"));
+                        vh.tv23.setTextColor(Color.parseColor("#3abfd1"));
+
+                }
+
+                vh.tv23.setText(itemList.get(2).getUsetypename());
+                switch (itemList.get(2).getUsetypename().trim()) {
+                    case "公司":
+                        vh.parent3.setBackgroundColor(Color.BLUE);
+                        break;
+                    case "库房":
+                        vh.parent3.setBackgroundColor(Color.YELLOW);
+                        break;
+                    case "宿舍":
+                        vh.parent3.setBackgroundColor(Color.CYAN);
+                        break;
+                    case "":
+                        vh.tv23.setText("---");
+                        vh.parent3.setBackgroundColor(Color.RED);
+                        break;
+
+                }
                 vh.tv3.setOnClickListener(new OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         Toast.makeText(context, itemList.get(2).getRoomNumber(), Toast.LENGTH_SHORT).show();
                     }
                 });
-            }else{
+            } else {
                 vh.tv3.setVisibility(View.INVISIBLE);
+                vh.tv13.setVisibility(View.INVISIBLE);
+                vh.tv23.setVisibility(View.INVISIBLE);
+
             }
+
+            if (itemList.size() > 3) {
+                vh.tv4.setVisibility(View.VISIBLE);
+
+                vh.tv14.setVisibility(View.VISIBLE);
+                vh.tv24.setVisibility(View.VISIBLE);
+                vh.tv4.setText(itemList.get(3).getRoomNumber());
+
+                vh.tv14.setText(itemList.get(3).getStatusname());
+                switch (itemList.get(3).getStatusname().trim()) {
+                    case "自用":
+                        vh.tv4.setTextColor(Color.GREEN);
+                        vh.tv14.setTextColor(Color.GREEN);
+                        vh.tv24.setTextColor(Color.GREEN);
+                        break;
+                    case "出租":
+                        vh.tv4.setTextColor(Color.GRAY);
+                        vh.tv14.setTextColor(Color.GRAY);
+                        vh.tv24.setTextColor(Color.GRAY);
+                        break;
+                    case "自住":
+                        vh.tv4.setTextColor(Color.LTGRAY);
+                        vh.tv14.setTextColor(Color.LTGRAY);
+                        vh.tv24.setTextColor(Color.LTGRAY);
+                        break;
+                    case "":
+                        vh.tv14.setText("---");
+                        vh.tv4.setTextColor(Color.WHITE);
+                        vh.tv14.setTextColor(Color.WHITE);
+                        vh.tv24.setTextColor(Color.WHITE);
+                        break;
+                    default:
+                        vh.tv4.setTextColor(Color.parseColor("#3abfd1"));
+                        vh.tv14.setTextColor(Color.parseColor("#3abfd1"));
+                        vh.tv24.setTextColor(Color.parseColor("#3abfd1"));
+
+
+                }
+
+                vh.tv24.setText(itemList.get(3).getUsetypename());
+                switch (itemList.get(3).getUsetypename().trim()) {
+
+                    case "公司":
+                        vh.parent4.setBackgroundColor(Color.BLUE);
+                        break;
+                    case "库房":
+                        vh.parent4.setBackgroundColor(Color.YELLOW);
+                        break;
+                    case "宿舍":
+                        vh.parent4.setBackgroundColor(Color.CYAN);
+                        break;
+                    case "":
+                        vh.tv24.setText("---");
+                        vh.parent4.setBackgroundColor(Color.RED);
+                        break;
+
+                }
+                vh.tv4.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(context, itemList.get(3).getRoomNumber(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else {
+                vh.tv4.setVisibility(View.INVISIBLE);
+                vh.tv14.setVisibility(View.INVISIBLE);
+                vh.tv24.setVisibility(View.INVISIBLE);
+            }
+        }
+        else
+        {
+Log.d("cc","caxacac");
+        }
         }
         return convertView;
     }
@@ -109,6 +362,19 @@ public class MyAdapter extends BaseAdapter{
         TextView tv1;
         TextView tv2;
         TextView tv3;
+        TextView tv4;
+        TextView tv11;
+        TextView tv12;
+        TextView tv13;
+        TextView tv14;
+        TextView tv21;
+        TextView tv22;
+        TextView tv23;
+        TextView tv24;
+        RelativeLayout parent;
+        RelativeLayout parent2;
+        RelativeLayout parent3;
+        RelativeLayout parent4;
 
     }
 }
