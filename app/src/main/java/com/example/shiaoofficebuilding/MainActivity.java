c2eb7b3ac73144380f7654123b80a17a8d254adb
LeoMachine <1962976634@qq.com>
2019-04-03 17:51:29 +0800

    listview

--- app/src/main/java/com/example/shiaoofficebuilding/MainActivity.java
+++ app/src/main/java/com/example/shiaoofficebuilding/MainActivity.java
@@ -31,9 +31,9 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     TextView tv;
     private ListView roomlv;
-    private List<roomlistAdapterInfo> roomInfoForAdapter;
+    private ArrayList<roomlistAdapterInfo> roomInfoForAdapter;
     private  String count;
-
+    MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         roomInfoForAdapter = new ArrayList<roomlistAdapterInfo>();
         roomlv = findViewById(R.id.searchroomlv);
         sendRequest();//get方法请求获取数据
+
     }
 
     private void sendRequest() {
@@ -118,198 +119,17 @@ public class MainActivity extends AppCompatActivity {
                 mapx.setStatusname(statusname);
                 mapx.setUsetypename(usetypename);
                 roomInfoForAdapter.add(mapx);
-
-                roomlv.setAdapter(new MainActivity.MyAdapter());
+                adapter = new MyAdapter(MainActivity.this,R.layout.showroomlist,roomInfoForAdapter);
+                roomlv.setAdapter(adapter);
             }
         });
     }
 
 
 //数据适配器
-//    private class MyAdapter extends BaseAdapter {
-//        int sumCount,count;
-//        MyAdapter(String count1) {
-//            count = Integer.parseInt(count1);
-//        }
-//        @Override
-//        public int getCount() {
-//
-//            if (count % 3 == 0) {
-//                sumCount = count / 3; // 如果是双数直接减半
-//            } else {
-//                sumCount = (int) Math.floor((double) count / 3) + 1;
-//            }
-//            return sumCount;
-//
-//        }
-//
-//        @Override
-//        public Object getItem(int position) {
-//            return position;
-//        }
-//
-//        @Override
-//        public long getItemId(int position) {
-//            return 0;
-//        }
-//        @Override
-//        public View getView(int position, View convertView, ViewGroup parent) {
-//
-//            View view = View.inflate(MainActivity.this, R.layout.showroomlist, null);
-//
-//            TextView roomnum = view.findViewById(R.id.roomnum1);
-//            TextView statusnum = view.findViewById(R.id.statusname1);
-//            TextView usertypename = view.findViewById(R.id.usetypename1);
-//            roomnum.setText(roomInfoForAdapter.get(position).getRoomNumber());
-//            statusnum.setText(roomInfoForAdapter.get(position).getStatusname());
-//            usertypename.setText(roomInfoForAdapter.get(position).getUsetypename());
-//          //  Log.d("aa",roomInfoForAdapter.get(0).getRoomNumber());
-//            return view;
-//
-//
-//        }
-//    }
-
-
-//    private class MyAdapter extends BaseAdapter {
-//        int sumCount,count,pos;
-//        MyAdapter(String count1,int Pos) {
-//            count = Integer.parseInt(count1);
-//            this.pos = Pos;
-//        }
-//        @Override
-//        public int getCount() {
-//
-//            if (count % 2 == 0) {
-//                sumCount = count / 2; // 如果是双数直接减半
-//            } else {
-//                sumCount = (int) Math.floor((double) count / 2) + 1;
-//            }
-//            return sumCount;
-//
-//        }
-//
-//        @Override
-//        public Object getItem(int position) {
-//            return position;
-//        }
-//
-//        @Override
-//        public long getItemId(int position) {
-//            return 0;
-//        }
-//        @Override
-//        public View getView(int position, View convertView, ViewGroup parent) {
-//
-//            View view1 = View.inflate(MainActivity.this, R.layout.showroomlist, null);
-////            View view1 = View.inflate(MainActivity.this, R.layout.showroomlist2, null);
-////            View view1 = View.inflate(MainActivity.this, R.layout.showroomlist3, null);
-//
-//            TextView roomnum1 = view1.findViewById(R.id.roomnum1);
-//            TextView statusnum1 = view1.findViewById(R.id.statusname1);
-//            TextView usertypename1 = view1.findViewById(R.id.usetypename1);
-//
-//            TextView roomnum2 = view1.findViewById(R.id.roomnum2);
-//            TextView statusnum2 = view1.findViewById(R.id.statusname2);
-//            TextView usertypename2 = view1.findViewById(R.id.usetypename2);
-//
-//            TextView roomnum3 = view1.findViewById(R.id.roomnum3);
-//            TextView statusnum3 = view1.findViewById(R.id.statusname3);
-//            TextView usertypename3 = view1.findViewById(R.id.usetypename3);
-//
-//
-//                Log.d("bb","pos  "+position);
-//
-//                if (position % 3 == 0) {
-//
-//                    roomnum1.setText(roomInfoForAdapter.get(position).getRoomNumber());
-//                    Log.d("aa",roomInfoForAdapter.get(position).getRoomNumber());
-//                    statusnum1.setText(roomInfoForAdapter.get(position).getUsetypename());
-//                    usertypename1.setText(roomInfoForAdapter.get(position).getStatusname());
-//                }
-//                else if (position % 3 == 1) {
-//
-//                    roomnum2.setText(roomInfoForAdapter.get(position*2).getRoomNumber());
-//                    statusnum2.setText(roomInfoForAdapter.get(position*2).getUsetypename());
-//                    usertypename2.setText(roomInfoForAdapter.get(position*2).getStatusname());
-//                }
-//                else if (position % 3 == 2) {
-//
-//                    roomnum3.setText(roomInfoForAdapter.get(position*3).getRoomNumber());
-//                    statusnum3.setText(roomInfoForAdapter.get(position*3).getUsetypename());
-//                    usertypename3.setText(roomInfoForAdapter.get(position*3).getStatusname());
-//                }
-//
-//            return view1;
-//
-//
-//        }
-//    }
-
-    public class MyAdapter extends BaseAdapter {
-
-
-        private int sumCount;
-
-
-        @Override
-        public int getCount() {
-            int count = roomInfoForAdapter.size();
-            if (count % 3 == 0) {
-                sumCount = count / 3; // 如果是双数直接减半
-            } else {
-                sumCount = (int) Math.floor((double) count / 3) + 1;
-            }
-            return sumCount;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return roomInfoForAdapter.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        class ViewHolder {
-            PayItem payitem1;
-            PayItem payitem2;
-            PayItem payitem3;
-        }
-
-        @SuppressLint("WrongViewCast")
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder holder = null;
-            if (convertView == null) {
-                holder = new ViewHolder();
-                convertView = LayoutInflater.from(MainActivity.this).inflate(R.layout.showroomlist, null);
-                holder.payitem1 = (PayItem)convertView.findViewById(R.id.roomnum1);
-                holder.payitem2 =  (PayItem)convertView.findViewById(R.id.roomnum2);
-                holder.payitem3 =  (PayItem)convertView.findViewById(R.id.roomnum3);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            holder.payitem1.setDescription(roomInfoForAdapter.get(position * 3).getRoomNumber());
-
-            if (position * 3 + 1 == roomInfoForAdapter.size()) {
-                holder.payitem2.setVisibility(View.INVISIBLE); // 如果是单数的话，那么最后一个item，右侧内容为空
-            } else {
-                holder.payitem2.setVisibility(View.VISIBLE); // 必须进行设置，负责存在复用holder的时候，会出现右侧的出现留白，跟最后一个一样，这个也是我写这篇文章最想锁
-
-
-            }
-
-            return convertView;
-        }
 
 
 
-    }
 
 
 }
---
+++ app/src/main/java/com/example/shiaoofficebuilding/roomlistAdapterInfo.java
@@ -0,0 +1,31 @@
+package com.example.shiaoofficebuilding;
+
+public class roomlistAdapterInfo {
+
+    //封装roomlist 获取到的数据
+    public String getRoomNumber() {
+        return RoomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        RoomNumber = roomNumber;
+    }
+
+    public String getStatusname() {
+        return statusname;
+    }
+
+    public void setStatusname(String statusname) {
+        this.statusname = statusname;
+    }
+
+    public String getUsetypename() {
+        return usetypename;
+    }
+
+    public void setUsetypename(String usetypename) {
+        this.usetypename = usetypename;
+    }
+
+    private String RoomNumber,statusname,usetypename;
+}
